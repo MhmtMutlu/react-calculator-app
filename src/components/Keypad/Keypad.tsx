4c3faa2a3fc055b3a0bf528d60be4fd7d54694cc
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-11-04 18:24:32 +0300

    Feat-UI: Keypad and Screen components were edited for CalcContext.

--- src/components/Keypad/Keypad.tsx
+++ src/components/Keypad/Keypad.tsx
@@ -1,26 +1,28 @@
 import React from 'react'
+import Button from '../Button/Button'
 
 const Keypad: React.FC = () => {
+
   return (
     <div className='calculator-keypad'>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button className='delete'>DEL</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button>/</button>
-      <button>X</button>
-      <button className='reset'>RESET</button>
-      <button className='equal'>=</button>
+      <Button isNumber={true} isOperator={false} value="7">7</Button>
+      <Button isNumber={true} isOperator={false} value="8">8</Button>
+      <Button isNumber={true} isOperator={false} value="9">9</Button>
+      <Button isNumber={false} isOperator={true} value="DEL" className='delete'>DEL</Button>
+      <Button isNumber={true} isOperator={false} value="4">4</Button>
+      <Button isNumber={true} isOperator={false} value="5">5</Button>
+      <Button isNumber={true} isOperator={false} value="6">6</Button>
+      <Button isNumber={false} isOperator={true} value="+">+</Button>
+      <Button isNumber={true} isOperator={false} value="1">1</Button>
+      <Button isNumber={true} isOperator={false} value="2">2</Button>
+      <Button isNumber={true} isOperator={false} value="3">3</Button>
+      <Button isNumber={false} isOperator={true} value="-">-</Button>
+      <Button isNumber={true} isOperator={false} value=".">.</Button>
+      <Button isNumber={true} isOperator={false} value="0">0</Button>
+      <Button isNumber={false} isOperator={true} value="/">/</Button>
+      <Button isNumber={false} isOperator={true} value="*">x</Button>
+      <Button isNumber={false} isOperator={true} value="RESET" className='reset'>RESET</Button>
+      <Button isNumber={false} isOperator={true} value="=" className='equal'>=</Button>
     </div>
   )
 }
--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CalcContext } from '../../contexts/CalcContext';
 
 const Screen: React.FC = () => {
+  const { mainText, screenText } = useContext(CalcContext);
   return (
-    <div className='calculator-screen'>12312</div>
+    <div className='calculator-screen'>
+      <div className='last-text'>{screenText}</div>
+      <div className='new-text'>{mainText}</div>
+    </div>
   )
 }
 
