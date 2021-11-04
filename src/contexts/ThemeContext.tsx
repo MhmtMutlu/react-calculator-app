16015d528fbac89db3dce1268574425981ca7fa7
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-11-04 18:38:49 +0300

    Feat: useState was changed with useLocalStorage to hold selected theme.

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 import { IContextProvider, IThemeContextInitialState } from "../types";
 
+const selectedTheme  = localStorage.getItem("theme")
+
 const initialContextState: IThemeContextInitialState = {
-  theme: "first-theme",
+  theme: selectedTheme != null ? JSON.parse(selectedTheme) : "first-theme",
   changeTheme: () => {},
 };
 
 export const ThemeContext = React.createContext(initialContextState);
 
 const ThemeContextProvider: React.FC<IContextProvider> = ({ children }) => {
-  const [theme, setTheme] = useState<string>(initialContextState.theme);
+  const [theme, setTheme] = useLocalStorage("theme", initialContextState.theme);
 
   const changeTheme = (newTheme: string) => {
     setTheme(newTheme);
